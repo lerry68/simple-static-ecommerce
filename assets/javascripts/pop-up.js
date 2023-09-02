c510cc41a23390274de0a47bd102afb7904749eb
lerry68 <farrelakiela68@yahoo.com>
2023-09-02 11:20:05 +0700

    push all

---
+++ assets/javascripts/pop-up.js
@@ -0,0 +1,70 @@
+// Created By: Farrel AKiela 
+
+function complain() {
+    window.alert("Terima Kasih, Masukkan Anda akan kami tampung");
+}
+
+function gbag() {
+    var product = "R - Tech Goodie Bag";
+    var price = "Rp. 50.000";
+
+    if(window.confirm("Anda akan membeli " +product+ "\ndengan total harga " +price+ "\n\nLanjutkan?"))
+    {
+        window.alert("Pesanan Anda akan segera di proses");
+    }
+    else {
+        window.alert("Pesanan Anda dibatalkan..");
+    }
+};
+
+function mug() {
+    var product = "R - Tech Mug";
+    var price = "Rp. 65.000";
+
+    if(window.confirm("Anda akan membeli " +product+ "\ndengan total harga " +price+ "\n\nLanjutkan?"))
+    {
+        window.alert("Pesanan Anda akan segera di proses");
+    }
+    else {
+        window.alert("Pesanan Anda dibatalkan..");
+    }
+};
+
+function cshirt() {
+    var product = "R - Tech Collared Shirt";
+    var price = "Rp. 100.000";
+
+    if(window.confirm("Anda akan membeli " +product+ "\ndengan total harga " +price+ "\n\nLanjutkan?"))
+    {
+        window.alert("Pesanan Anda akan segera di proses");
+    }
+    else {
+        window.alert("Pesanan Anda dibatalkan..");
+    }
+};
+
+function cap() {
+    var product = "R - Tech Cap";
+    var price = "Rp. 75.000";
+
+    if(window.confirm("Anda akan membeli " +product+ "\ndengan total harga " +price+ "\n\nLanjutkan?"))
+    {
+        window.alert("Pesanan Anda akan segera di proses");
+    }
+    else {
+        window.alert("Pesanan Anda dibatalkan..");
+    }
+};
+
+function clock() {
+    var product = "R - Tech Clock";
+    var price = "Rp. 55.000";
+
+    if(window.confirm("Anda akan membeli " +product+ "\ndengan total harga " +price+ "\n\nLanjutkan?"))
+    {
+        window.alert("Pesanan Anda akan segera di proses");
+    }
+    else {
+        window.alert("Pesanan Anda dibatalkan..");
+    }
+};
